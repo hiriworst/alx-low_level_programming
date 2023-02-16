45f627989e335c5e0d834f95cad8758bf811f5b5
hiriworst <saidhiribae@gmail.com>
2023-02-16 10:08:39 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main()
+/*
+ *  a program that say "Programming is like building a multilingual puzzle
+ *
+ *  it also should return 0
+ * /
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
