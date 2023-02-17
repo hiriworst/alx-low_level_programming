aa6cefa0fdeade4539d27da096c67255e5726e31
hiriworst <saidhiribae@gmail.com>
2023-02-17 12:26:26 -0800

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - assign a random number to the variable n each time it is executed
  * print the last digit of the number stored in the variable n
@@ -9,7 +8,7 @@
 int main(void)
 {
 	int n;
-	
+
 	for (n = 0; n < 10; n++)
 	{
 		printf("%d", n);
