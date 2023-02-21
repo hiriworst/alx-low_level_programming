664d6ae92029fec47854ac8d6b5f113d7e872855
hiriworst <saidhiribae@gmail.com>
2023-02-21 01:57:08 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print _putchar follwed by a newline
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch = "_putchar";
+
+	putchar (ch);
+
+	putchar ("\n");
+
+	return (0);
+
+}
